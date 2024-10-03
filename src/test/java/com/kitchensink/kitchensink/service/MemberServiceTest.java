2c9e8821c3e9b4f2e10ee15fd024e26cf4bf1053
Matej Pargac <matej.pargac@resmed.com>
2024-10-03 11:01:27 +0200

    Add test, context path

--- src/test/java/com/kitchensink/kitchensink/service/MemberServiceTest.java
+++ src/test/java/com/kitchensink/kitchensink/service/MemberServiceTest.java
@@ -11,10 +11,13 @@ import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Sort;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -56,6 +59,32 @@ public class MemberServiceTest {
         assertEquals(expected.getPhoneNumber(), actual.getPhoneNumber());
     }
 
+    @Test
+    public void getAllMembers() {
+        Long extId = 1L;
+        Member entity = Member.builder()
+                .extId(extId)
+                .name("name")
+                .email("email")
+                .phoneNumber("phoneNumber")
+                .build();
+        MemberDTO expected = MemberDTO.builder()
+                .id(extId)
+                .name("name")
+                .email("email")
+                .phoneNumber("phoneNumber")
+                .build();
+
+        when(memberRepository.findAll(any(Sort.class))).thenReturn(List.of(entity));
+        when(memberMapper.entityToDto(entity)).thenReturn(expected);
+
+        List<MemberDTO> actualList = memberService.getAllMembers(Sort.by(Sort.Direction.ASC, "name"));
+        assertEquals(1, actualList.size());
+        assertEquals(expected.getId(), actualList.get(0).getId());
+        assertEquals(expected.getName(), actualList.get(0).getName());
+        assertEquals(expected.getEmail(), actualList.get(0).getEmail());
+        assertEquals(expected.getPhoneNumber(), actualList.get(0).getPhoneNumber());
+    }
     @Test
     public void createMember() {
         Member entity = Member.builder()
